77228429e311ee547a4fa67e6692285b1c83c45f
ianagbip1oti <ianagbip1oti@gmail.com>
2021-01-12 01:44:10 +0000

    📦 NEW: Add websocket receiving and listening

--- src/examples/ping_bot.rs
+++ src/examples/ping_bot.rs
@@ -1,15 +1,12 @@
-use log::error;
+use log::info;
 use smalld_rust::SmallD;
-use std::process::exit;
 
 fn main() {
     pretty_env_logger::init();
 
-    match SmallD::new() {
-        Ok(smalld) => smalld.run(),
-        Err(e) => {
-            error!("Application error: {}", e);
-            exit(1);
-        }
-    }
+    let mut smalld: SmallD = SmallD::new().expect("Failed to initialize smalld");
+
+    smalld.on_gateway_payload(|p| info!("payload received in ping bot! {}", p));
+
+    smalld.run();
 }
