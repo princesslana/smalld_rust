184e4b91afdf0fcb14fa152ecc7024b075afd957
ianagbip1oti <ianagbip1oti@gmail.com>
2021-01-14 00:54:59 +0000

    📦 NEW: Send identify on hello

--- src/examples/ping_bot.rs
+++ src/examples/ping_bot.rs
@@ -1,12 +1,9 @@
-use log::info;
 use smalld_rust::SmallD;
 
 fn main() {
     pretty_env_logger::init();
 
-    let mut smalld: SmallD = SmallD::new().expect("Failed to initialize smalld");
-
-    smalld.on_gateway_payload(|p| info!("payload received in ping bot! {:?}", p));
+    let smalld: SmallD = SmallD::new().expect("Failed to initialize smalld");
 
     smalld.run();
 }
