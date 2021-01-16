0cabe2de431698ada391fbb02a3a1e13f5c8b978
ianagbip1oti <ianagbip1oti@gmail.com>
2021-01-16 00:35:28 +0000

    📦 NEW: Add Http#post. Working ping bot.

--- src/examples/ping_bot.rs
+++ src/examples/ping_bot.rs
@@ -1,15 +1,24 @@
-use log::debug;
+use log::{debug, warn};
+use serde_json::json;
 use smalld_rust::SmallD;
 
 fn main() {
     pretty_env_logger::init();
 
-    let mut smalld: SmallD = SmallD::new().expect("Failed to initialize smalld");
+    let mut smalld = SmallD::new().expect("Failed to initialize smalld");
+    let smalld_clone = smalld.clone();
 
-    smalld.on_event("MESSAGE_CREATE", |json| {
+    smalld.on_event("MESSAGE_CREATE", move |json| {
         if let Some("++ping") = json.get("content").and_then(|c| c.as_str()) {
-            // pong
             debug!("Pong!");
+            if let Some(channel_id) = json.get("channel_id").and_then(|c| c.as_str()) {
+                if let Err(err) = smalld_clone.post(
+                    format!("/channels/{}/messages", channel_id),
+                    json!({"content":"pong"}),
+                ) {
+                    warn!("Error sending pong response: {}", err);
+                }
+            }
         }
     });
 
